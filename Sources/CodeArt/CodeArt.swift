5df6e811444b04876a16fc65f8cf867d89491f8e
Willian Guedes da Silva Cunha <willian.staiper@gmail.com>
2020-11-07 15:43:34 -0300

    Providing static variable for a color palette.

--- Sources/CodeArt/CodeArt.swift
+++ Sources/CodeArt/CodeArt.swift
@@ -1,7 +1,12 @@
 import Foundation
 import UIKit
 
-class CodeArt {
+public class CodeArt {
+    
+    /// The most eye-pleasing color known to all humanity
+    public static var codeArtColor: UIColor {
+        return self.colorFromHexString(hex: "006736")
+    }
     
     /// Allows you to convert a 6 digit hexadecimal string into a UIColor instance
     /// - Warning: The '#' symbol is stripped from the beginning of the string submitted here
@@ -9,7 +14,7 @@ class CodeArt {
     ///   - hex: A 6-digit hexadecimal string. Use 6 digits rather than 8, and add the accompanying alpha value in the second parameter
     ///   - alpha: A number between 0.0 and 1.0 indicating how transparente the color is
     /// - Returns: A UIColor defined by the 'hex' parameter
-    class func colorFromHexString (hex: String, alpha: CGFloat = 1.0) -> UIColor {
+    internal class func colorFromHexString (hex: String, alpha: CGFloat = 1.0) -> UIColor {
         let r, g, b: CGFloat
         let offset = hex.hasPrefix("#") ? 1 : 0
         let start = hex.index(hex.startIndex, offsetBy: offset)
--- Tests/CodeArtTests/CodeArtTests.swift
+++ Tests/CodeArtTests/CodeArtTests.swift
@@ -6,8 +6,14 @@ final class CodeArtTests: XCTestCase {
         let color = CodeArt.colorFromHexString(hex: "FF0000", alpha: 1.0)
         XCTAssertEqual(color, .red)
     }
+    
+    func testCodeArtEqual() {
+        let color = CodeArt.colorFromHexString(hex: "006736")
+        XCTAssertEqual(color, CodeArt.codeArtColor)
+    }
 
     static var allTests = [
         ("testColorPallete", testColorPallete),
+        ("testCodeArtEqual",testCodeArtEqual),
     ]
 }
